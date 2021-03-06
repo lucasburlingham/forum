d2f3fdb1791fe0cc0db71317201cb42ad0a1bf3d
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-05 23:24:18 -0500

    change around configuration

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_SERVER', '195.201.179.80:2222');
-// define('DB_USERNAME', 'xenonmcx_agent');
-define('DB_USERNAME', 'test');
-define('DB_PASSWORD', 'test');
-// define('DB_PASSWORD', 'wVoiwdt1');
+define('DB_SERVER', '195.201.179.80');
+define('DB_USERNAME', 'xenonmcx_agent');
+// define('DB_USERNAME', 'test');
+// define('DB_PASSWORD', 'test');
+define('DB_PASSWORD', 'wVoiwdt1');
 define('DB_NAME', 'xenonmcx_xframe');
 
 /* Attempt to connect to MySQL database */
