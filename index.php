9ecf2c128042be9b7bde4b9adf68a98065c61e12
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-09 21:04:06 -0500

    asdf

--- index.php
+++ index.php
@@ -343,15 +343,181 @@ if (isset($_GET['ID'])) {
 function displayPost($topic_ID)
 {
     require_once "config.php";
-    echo "The topic ID is: " . $topic_ID . "<br>";
-    $sql = "SELECT * FROM xenonmcx_xframe.content where `topic_ID` = 1";
-    echo $sql . "<br>";
-
-    echo 'Connected successfully<br>';
+    $sql = "SELECT * FROM xenonmcx_xframe.content where `topic_ID` = $topic_ID";
     $result = mysqli_query($conn, $sql);
-    // echo "Result: " . $result . "<br>";
     $row = mysqli_fetch_assoc($result);
-    var_dump($result);
-    echo mysqli_error($conn);
-    echo $row["topic_CONTENT"];
-}
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Description" content="Homepage of the XENONMC Development Team. Meet our team members and connect to our growing community." />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" async>
+        <!-- load sheet then javascript -->
+        <link rel="stylesheet/less" type="text/css" href="assets/css/styles.less" />
+        <script src="//cdn.jsdelivr.net/npm/less@3.13"></script>
+        <title>XForum: <?php echo $row["topic_TITLE"]; ?></title>
+        <link rel="shortcut icon" href="assets/images/logo.jpg" type="image/x-icon">
+    </head>
+
+    <body class="bg-carbon">
+        <section id="navbar">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/images/logo.jpg" alt="XENONMC Logo: Green Lightning Bolt on a Black background">
+                    XForum</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#topNav" aria-controls="topNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="topNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/index.php">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#teamMembers_centered">Our Team</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="contactDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Links & Contact</a>
+                            <div class="dropdown-menu bg-dark" aria-labelledby="contactDropdown">
+                                <a class="dropdown-item text-discord" href="https://discord.gg/jFEWYnDW6y"><i class="fab fa-discord"></i> Discord</a>
+                                <a class="dropdown-item text-github" href="https://github.com/xenonmc-dev"><i class="fab fa-github-square"></i> Github</a>
+                            </div>
+                        </li>
+                        <?php
+                        if (isset($_SESSION["username"])) {
+                        ?>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="accountDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo htmlspecialchars($_SESSION["username"]); ?></a>
+                                <div class="dropdown-menu bg-dark" aria-labelledby="accountDropdown">
+                                    <a href="profile.php" class="btn btn-info">Personal Profile</a>
+                                    <a href="settings.php" class="btn btn-info">Manage Your Settings</a>
+                                    <hr>
+                                    <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+                                </div>
+                            </li>
+                        <?php
+                        } else {
+                        ?>
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-outline-success" href="login.php">Login </a>
+                            </li>
+                        <?php }
+                        ?>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+
+
+
+        <section id="footer">
+            <footer class="bg-dark padding-up padding-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4 col-lg-4">
+                            <h5>XenonMC Development Team</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4 col-lg-4">
+                            <ul class="no-style">
+                                <p>
+                                    <b>Links:</b>
+                                </p>
+                                <li>
+                                    <a class="text-purple" href="https://discord.gg/jFEWYnDW6y"><i class="fab fa-discord"></i>
+                                        Discord</a>
+                                </li>
+                                <li>
+                                    <a class="text-light" href="https://github.com/xenonmc-dev"><i class="fab fa-github-square"></i> Github</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-12 col-md-5 col-lg-5">
+                            <table class="padding-bottom">
+                                <tr>
+                                    <th>
+                                        <b>Status:</b>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <small>
+                                            Codacy Review:
+                                        </small>
+                                    </td>
+                                    <td>
+                                        <a href="https://www.codacy.com/gh/XENONMC-DEV/xenonmc.xyz/dashboard?utm_source=github.com&amp;utm_content=XENONMC-DEV/xenonmc.xyz">
+                                            <img src="https://app.codacy.com/project/badge/Grade/00257b7c0557468fb18de75285bbfe44" />
+                                        </a>
+                                    </td>
+                                <tr>
+                                    <td>
+                                        <small>
+                                            IONOS Status:
+                                        </small>
+                                    </td>
+                                    <td>
+                                        <statuspage-widget src="https://www.ionos-status.com" appearance="badge">
+                                        </statuspage-widget>
+                                    </td>
+                                </tr>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <small>Github Status:</small>
+                                    </td>
+                                    <td>
+                                        <statuspage-widget src="https://www.githubstatus.com" appearance="badge">
+                                        </statuspage-widget>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <small>Overall Status:</small>
+                                    </td>
+                                    <td>
+                                        <statuspage-widget src="https://xenonmc.statuspage.io/" appearance="badge">
+                                        </statuspage-widget>
+                                    </td>
+                                </tr>
+                            </table>
+
+                        </div>
+                        <div class="col-sm-12 col-md-3 col-lg-3">
+                            <b>
+                                &copy; 2021 <a href="https://lucasburlingham.me/" class="text-warning">Lucas
+                                    Burlingham</a> and the
+                                <a href="#navbar" class="text-success">XenonMC Development Team</a>
+                            </b>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </section>
+        <!-- discord integration -->
+        <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+            new Crate({
+                server: '765395501331120168',
+                channel: '800154146434056192',
+            })
+        </script>
+        <!-- Statuspage  -->
+        <script src="https://rpl357kc8bzh.statuspage.io/embed/script.js"></script>
+        <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.1.3/webcomponents-bundle.js"></script>
+        <script src="https://unpkg.com/@statuspage/status-widget/dist/index.js"></script>
+
+        <!-- bootstrap dependencies -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
+<?php }
