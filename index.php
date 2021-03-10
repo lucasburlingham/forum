e2c2040fc356098aafe1e101a40d7b8d837c54ca
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-09 22:45:48 -0500

    asdf

--- index.php
+++ index.php
@@ -415,12 +415,12 @@ function displayPost($topic_ID)
         </section>
 
         <section id="template">
-            <div class="container">
+            <div class="container padding-top">
                 <div class="row">
                     <div class="col-sm-1 col-md-3">
                     </div>
                     <div class="col-sm-10 col-md-6">
-                        <div class="card">
+                        <div class="card bg-dark">
                             <div class="card-header">
                                 <h5 class="card-title"><?php echo $row["topic_NAME"]; ?></h5>
                                 <small><?php echo $row["topic_DATE"]; ?></small>
