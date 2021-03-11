ec5745fdaabe9797fe0cef933812ba9400c7a94e
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-10 21:12:29 -0500

    asdf

--- getAllPosts.php
+++ getAllPosts.php
@@ -3,14 +3,54 @@ require_once "config.php";
 $result = mysqli_query($conn, "SELECT * FROM `content` ORDER BY `topic_DATE` DESC;");
 
 while ($row = mysqli_fetch_array($result)) {
-    echo $row['topic_NAME'];
-    echo "<br>";
-    echo $row['topic_DATE'];
-    echo "<br>";
-    echo $row['topic_CONTENT'];
-    echo "<br>";
-    echo $row['topic_REP'];
-    echo "<br>";
-    echo $row['topic_CREATORID'];
-    echo "<br>";
+
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Description" content="Homepage of the XENONMC Development Team. Meet our team members and connect to our growing community." />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" async>
+        <!-- load sheet then javascript -->
+        <link rel="stylesheet/less" type="text/css" href="assets/css/styles.less" />
+        <script src="//cdn.jsdelivr.net/npm/less@3.13"></script>
+        <title>XForum: <?php echo $row["topic_NAME"]; ?></title>
+        <link rel="shortcut icon" href="assets/images/logo.jpg" type="image/x-icon">
+    </head>
+
+    <body class="bg-carbon">
+        <section id="template">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-1 col-md-2">
+                    </div>
+                    <div class="col-sm-10 col-md-8">
+                        <div class="card bg-dark my-5">
+                            <div class="card-header">
+                                <h5 class="card-title text-success"><?php echo $row["topic_NAME"]; ?></h5>
+                                <small><?php
+                                        $date = date_create($row["topic_DATE"]);
+                                        echo date_format($date, 'jS F Y \a\t g:ia'); ?></small>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text"><?php echo $row["topic_CONTENT"]; ?></p>
+                            </div>
+                            <div class="card-footer">
+                                <small>Reputation: <?php echo $row["topic_REP"]; ?></small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-1 col-md-2">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+
+    </html>
+
+<?php
 }
