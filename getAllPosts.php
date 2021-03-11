ff368725ca441f9da9c93e33c45b874ca72dd24f
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-10 21:09:16 -0500

    asdf

--- getAllPosts.php
+++ getAllPosts.php
@@ -4,8 +4,13 @@ $result = mysqli_query($conn, "SELECT * FROM `content` ORDER BY `topic_DATE` DES
 
 while ($row = mysqli_fetch_array($result)) {
     echo $row['topic_NAME'];
+    echo "<br>";
     echo $row['topic_DATE'];
+    echo "<br>";
     echo $row['topic_CONTENT'];
+    echo "<br>";
     echo $row['topic_REP'];
+    echo "<br>";
     echo $row['topic_CREATORID'];
+    echo "<br>";
 }
