f5cfc461a95123bf0f4e92e712e44920cf45dba5
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-10 21:03:45 -0500

    sdfg

---
+++ getAllPosts.php
@@ -0,0 +1,11 @@
+<?php
+require_once "config.php";
+$result = mysqli_query($conn, "SELECT * FROM `xenonmcx_forum.content` ORDER BY `topic_DATE` DESC;");
+
+while ($row = mysqli_fetch_array($result)) {
+    echo $row['topic_NAME'];
+    echo $row['topic_DATE'];
+    echo $row['topic_CONTENT'];
+    echo $row['topic_REP'];
+    echo $row['topic_CREATORID'];
+}
